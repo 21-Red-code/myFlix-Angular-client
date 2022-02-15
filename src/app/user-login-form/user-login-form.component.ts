3b946d79cc8c8ae6343f5d45c437f8e78279284f
21-Red-code <shermang37@gmail.com>
2022-02-15 14:05:35 +0100

    Adding two user-registration and login components

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog'; 
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Send a request to login the user
+   * Saves the token and username in localSotrage
+   * Once logged in, re-route to movies page 
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      console.log(response);
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('username', response.user.Username);
+      this.snackBar.open('User login successful!', 'OK', {
+        duration: 2000
+      });
+      this.router.navigate(['movies']);
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
